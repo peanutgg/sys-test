274259ce348db7a13315b3fa7a62583988ebb633
xiaodong.xie <xiaodong.xie@lepudigital.com>
2022-04-27 16:11:30 +0800

    created serivce a add openfeign

---
+++ sys-test-service-a/src/main/java/com/sys/test/service/a/SysTestServiceAApplication.java
@@ -0,0 +1,13 @@
+package com.sys.test.service.a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysTestServiceAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysTestServiceAApplication.class, args);
+    }
+
+}
