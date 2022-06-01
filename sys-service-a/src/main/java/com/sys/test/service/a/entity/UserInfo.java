b7147f2477f766837563219acd0d986fd01f9713
xiaodong.xie <xiaodong.xie@lepudigital.com>
2022-06-01 09:03:37 +0800

    1

---
+++ sys-service-a/src/main/java/com/sys/test/service/a/entity/UserInfo.java
@@ -0,0 +1,11 @@
+package com.sys.test.service.a.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserInfo {
+    private Integer id;
+    private String name;
+}
