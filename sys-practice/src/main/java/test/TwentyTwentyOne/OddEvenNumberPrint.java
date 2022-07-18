405f86254a434a155fad13eb51f446b719ed0dc0
xiaodong.xie <xiaodong.xie@lepudigital.com>
2022-07-18 13:40:15 +0800

    condition await signal

--- sys-practice/src/main/java/test/TwentyTwentyOne/OddEvenNumberPrint.java
+++ sys-practice/src/main/java/test/TwentyTwentyOne/OddEvenNumberPrint.java
@@ -1,6 +1,8 @@
 package test.TwentyTwentyOne;
 
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class OddEvenNumberPrint {
     public static void main(String[] args) {
@@ -8,46 +10,63 @@ public class OddEvenNumberPrint {
         Thread t2;
         Thread t1;
         Object lock = new Object();
+        ReentrantLock rlock = new ReentrantLock();
+        Condition c1 = rlock.newCondition();
         Semaphore tk = new Semaphore(1);
         t1 = new Thread(() -> {
-            synchronized (lock) {
+            rlock.lock();
+//            synchronized (lock) {
                 System.out.println("t1获得锁开始执行");
+            try {
                 while (i[0] < 100) {
                     if (i[0] % 2 == 1) {
                         System.out.println(i[0] + "\r");
                         i[0]++;
-
+                        c1.signal();
 //                        lock.notify();
-//                        System.out.println("t1通知完成释放锁");
-                    }else{
+                        System.out.println("t1通知完成");
+                    } else {
                         try {
-//                            System.out.println("t1 wait");
-                            lock.wait();
+                            System.out.println("t1 wait");
+                            c1.await();
+//                            lock.wait();
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                     }
                 }
+//            }
+            } finally {
+                rlock.unlock();
+                System.out.println("t1 release");
             }
         });
         t2 = new Thread(() -> {
-            synchronized (lock) {
+            rlock.lock();
+//            synchronized (lock) {
+            try {
                 System.out.println("t2获得锁开始执行");
                 while (i[0] < 100) {
                     if (i[0] % 2 == 0) {
                         System.out.println(i[0] + "\r");
                         i[0]++;
-                        lock.notify();
-//                        System.out.println("t2通知完成释放锁");
-                    }else{
+                        c1.signal();
+//                        lock.notify();
+                        System.out.println("t2通知完成");
+                    } else {
                         try {
-//                            System.out.println("t2 wait");
-                            lock.wait();
+                            System.out.println("t2 wait");
+                            c1.await();
+//                            lock.wait();
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                     }
                 }
+//            }
+            } finally {
+                rlock.unlock();
+                System.out.println("t2 release");
             }
         });
         t1.start();
