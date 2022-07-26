85978262194b183135b558700cbe438d4e893b51
peanutgghope <dongxiaoxie_321>
2022-07-26 18:02:06 +0800

    test juc

--- sys-executor-pool/src/main/java/com/sys/executor/AAA.java
+++ sys-executor-pool/src/main/java/com/sys/executor/AAA.java
@@ -4,10 +4,29 @@ import lombok.SneakyThrows;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Phaser;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+import java.util.concurrent.atomic.AtomicMarkableReference;
+import java.util.concurrent.atomic.AtomicStampedReference;
+import java.util.concurrent.locks.LockSupport;
 
 public class AAA {
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(1);
+        AtomicInteger a1 = new AtomicInteger(1);
+        a1.set(1);
+        a1.get();
+
+
+        AtomicStampedReference a2 = new AtomicStampedReference(1);
+        a2.set(1,1);
+        a2.set(2, 2);
+
+        AtomicIntegerFieldUpdater<User> a4 = AtomicIntegerFieldUpdater.newUpdater(User.class, "id");
+        a4.set(new User(), 1);
+        a4.getAndIncrement(new User());
+
 
         executorService.execute(new Runnable() {
             @SneakyThrows
