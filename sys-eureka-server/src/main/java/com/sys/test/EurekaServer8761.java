65ab17a7301a133bbea723ecaf932f6932b563d5
xiaodong.xie <xiaodong.xie@lepudigital.com>
2022-08-31 07:03:49 +0800

    fix: eureka client server version error

--- sys-eureka-server/src/main/java/com/sys/test/EurekaServer8761.java
+++ sys-eureka-server/src/main/java/com/sys/test/EurekaServer8761.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 import java.io.IOException;
@@ -15,6 +16,7 @@ import java.io.IOException;
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @EnableEurekaServer
 @EnableDiscoveryClient
+@EnableEurekaClient
 public class EurekaServer8761 {
 
     public static void main(String[] args) throws IOException {
