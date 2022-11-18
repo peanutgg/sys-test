7ccc54a22b1a304d59adbf86c3bd5e80595a9fd4
xiexiaodong <xiexd@zhifa.cn>
2022-11-18 09:58:51 +0800

    add java 8 merge test

--- sys-practice/src/main/java/test/practice/list/Test.java
+++ sys-practice/src/main/java/test/practice/list/Test.java
@@ -18,5 +18,19 @@ public class Test {
         for (int i = 0; i < arr.getSize(); i++) {
             System.out.println(arr.get(i));
         }
+
+        Map<String, Integer> m1 = new HashMap<>();
+        Map<String, Integer> m2 = new HashMap<>();
+        m1.put("k1", 1);
+        m1.put("k2", 2);
+        m1.put("k3", 3);
+        m1.put("k5", 5);
+        m2.put("k1", 1);
+        m2.put("k2", 2);
+        m2.put("k3", 3);
+        m2.put("k4", 4);
+        m1.forEach((key, value) -> m2.merge(key, value, (v1, v2) -> m1.get(key) + m2.get(key)));
+        m2.forEach((k, v) -> System.out.println(k + ":" + v));
     }
+
 }
